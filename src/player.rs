b116356cb6f2e1f9001dda15b7557ad1d09ee383
Xinyuan Zhao <22809191+Bichidian@users.noreply.github.com>
2024-11-26 16:15:48 +0800

    Implement a smarter bot

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,7 @@
 use crate::action::{Action, Resource};
 use crate::game::Play;
+use rand;
+use rand::seq::{IteratorRandom, SliceRandom};
 use std::io;
 
 pub struct CLIPlayer;
@@ -27,10 +29,27 @@ pub struct BotPlayer;
 
 impl Play for BotPlayer {
     fn get_action(&self, state: &Resource, other_state: &Resource) -> Action {
-        if state[0] == 0 {
-            Action::Guahao
-        } else {
-            Action::Attack(1)
+        let mut rng = rand::thread_rng();
+        let sensible_actions = self.list_sensible_actions(&state, &other_state);
+        sensible_actions.into_iter().choose(&mut rng).unwrap()
+    }
+}
+
+impl BotPlayer {
+    fn list_sensible_actions(&self, state: &Resource, other_state: &Resource) -> Vec<Action> {
+        let mut sensible_actions = vec![Action::Guahao];
+        for a in 1..=state[0] {
+            sensible_actions.push(Action::Attack(a as u8));
         }
+        for d in 1..=other_state[0] {
+            sensible_actions.push(Action::Defend(d as u8));
+        }
+        if other_state[0] > 1 && state[1] > 0 {
+            sensible_actions.push(Action::Quanfang);
+        }
+        if other_state[0] > 0 && state[2] > 0 {
+            sensible_actions.push(Action::Fantan);
+        }
+        sensible_actions
     }
 }
